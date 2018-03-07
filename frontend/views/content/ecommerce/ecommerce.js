4d094199869af30c0836962d55c073c8fd5a52e9
Srujana <srujana.vemula@wohlig.com>
2018-03-07 19:14:32 +0530

    added checkboc custom to pricing card and made banner

--- frontend/views/content/ecommerce/ecommerce.js
+++ frontend/views/content/ecommerce/ecommerce.js
@@ -8,20 +8,20 @@ myApp.controller('EcommerceCtrl', function ($scope, TemplateService, NavigationS
         "period": "Month",
         "minper": "Minimum Period 12 months",
         "otp": "Rs 5,99,999 for One Time Payment*",
-        "services":["Ecommerce Website","Mobile App Android","Design service (Banners)","Couriers Solution","Advertisement on FB / Google","Email &amp; SMS Marketing","Social Media Management","Marketplace Advertisement","Personal Account Manager"]
+        "services":["Ecommerce Website","Mobile App Android","Design service (Banners)","Couriers Solution","Advertisement on FB / Google","Email & SMS Marketing","Social Media Management","Marketplace Advertisement","Personal Account Manager"]
     },{
         "name": "Professional",
         "price": "₹ 75,000",
         "period": "Month",
         "minper": "Minimum Period 12 months",
         "otp": "Rs 8,99,999 for One Time Payment*",
-        "services":["Ecommerce Website","Mobile App Android + iOS","Design service (Banners)","Couriers Solution","Advertisement on FB / Google","Email &amp; SMS Marketing","Social Media Management","Marketplace Advertisement","Personal Account Manager"]
+        "services":["Ecommerce Website","Mobile App Android + iOS","Design service (Banners)","Couriers Solution","Advertisement on FB / Google","Email & SMS Marketing","Social Media Management","Marketplace Advertisement","Personal Account Manager"]
     },{
         "name": "Premium",
         "price": "₹ 1,00,000",
         "period": "Month",
         "minper": "Minimum Period 12 months",
         "otp": "Rs 9,99,999 for One Time Payment*",
-        "services":["Ecommerce Website","Mobile App Android + iOS","Design service (Banners)","Couriers Solution","Advertisement on FB / Google","Email &amp; SMS Marketing","Social Media Management","Marketplace Advertisement","Personal Account Manager"]
+        "services":["Ecommerce Website","Mobile App Android + iOS","Design service (Banners)","Couriers Solution","Advertisement on FB / Google","Email & SMS Marketing","Social Media Management","Marketplace Advertisement","Personal Account Manager"]
     }]
 })
