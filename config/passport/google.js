e089892cee40bcd48efe82d0ea5b9bd03add36d1
snehalthakare <snehal.thakare@wohlig.com>
2018-04-18 16:24:53 +0530

    changes

--- config/passport/google.js
+++ config/passport/google.js
@@ -1,6 +1,6 @@
 global["GoogleKey"] = "AIzaSyDMgwXi6D38isibUfShc9C2mJyaHZZ2LpE";
-global["GoogleclientId"] = "529279279497-hdm2ul03erq4kitk7qlqbf41h6pl8f7p.apps.googleusercontent.com";
-global["GoogleclientSecret"] = "nNZiqXW5U2364QI9--sVIR8B";
+global["GoogleclientId"] = "1066475903870-shb166fa48qktat3e1cqc9cjgjvcs301.apps.googleusercontent.com";
+global["GoogleclientSecret"] = "X7eubdh5KexoyDW5UXcAvw6D";
 
 passport.use(new GoogleStrategy({
         clientId: GoogleclientId,
