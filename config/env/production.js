cd5f437076e1a186733d8f7d33f12b5f0761c425
Chintan Shah <chintan@wohlig.com>
2018-04-18 14:11:44 +0530

    Done

--- config/env/production.js
+++ config/env/production.js
@@ -26,7 +26,7 @@ module.exports = {
    ***************************************************************************/
 
   port: 1337,
-  realHost: "http://wohlig.io:1337",
+  realHost: "https://shopmyway.co.in",
   emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com"]
 
   /***************************************************************************
